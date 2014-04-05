d6bd10cdde898fbb9cf27eedc8b3815951284f47
Михаил Милушев <lanzz@lanzz.org>
2014-04-05 23:26:51 +0300

    pubic symbols in query/__init__.py and sql/__init__.py

--- sqlbuilder/query/__init__.py
+++ sqlbuilder/query/__init__.py
@@ -5,5 +5,7 @@ SQL queries
 """
 
 from __future__ import absolute_import
+from ..sql.expression import CASE
 from ..sql.name import C, F, T, V
+from ..sql.query import VALUES
 from .select import SELECT
---
+++ sqlbuilder/sql/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+SQL syntax
+"""
+
+from __future__ import absolute_import
+from .expression import CASE
+from .name import C, F, T, V
+from .query import VALUES
