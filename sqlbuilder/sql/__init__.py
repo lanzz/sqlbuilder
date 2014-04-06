d0c393cf039d79bb6e8cba7bd42adbfac5de8ed6
Михаил Милушев <lanzz@lanzz.org>
2014-04-07 00:17:40 +0300

    shorthand `L` for turning literals into expressions example usage: `SELECT(L('constant string').AS('alias'))`

--- sqlbuilder/sql/__init__.py
+++ sqlbuilder/sql/__init__.py
@@ -6,5 +6,5 @@ SQL syntax
 
 from __future__ import absolute_import
 from .name import C, F, T, V, ONLY
-from .expression import CASE
+from .expression import CASE, Value as L
 from .query import VALUES
